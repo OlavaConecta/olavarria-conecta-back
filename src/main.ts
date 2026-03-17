90146ff16cb88042bdd3eaaa53dde9a143fcc659
Juan <juanccamposrey@gmail.com>
2026-03-17 00:15:47 -0300

    Arreglo back 17/03

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create <NestExpressApplication>(AppModule);
@@ -16,7 +17,11 @@ async function bootstrap() {
     credentials:true,
   }
 )
-
+app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, // ESTO es lo que hace que el planId pase de "1" a 1
+  }));
 app.use(json({ limit: '50mb' })); 
   app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT ?? 3000);
