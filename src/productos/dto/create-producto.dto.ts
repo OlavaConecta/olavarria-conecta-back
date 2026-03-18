9f01e54fed31816691098c5a5f0fe656b6fd9f90
Juan <juanccamposrey@gmail.com>
2026-03-17 21:39:50 -0300

    se arreglo el dto de create producto en imagen

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateProductoDto {
 
@@ -14,6 +14,7 @@ export class CreateProductoDto {
     @IsNumber()
     precio: number;
 
+    @IsOptional()
     @IsString()
     imagen:string;
 
