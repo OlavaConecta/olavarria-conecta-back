6f56e939f234d6dad9948b39d5d4032182d7e373
Juan Campos Rey <juanccamposrey@gmail.com>
2026-04-21 01:44:57 -0300

    se corrigio dto

--- src/mail/dto/create-mail.dto.ts
+++ src/mail/dto/create-mail.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateMailDto {
     nombre:string;
+
+    nombreLocal:string;
+    @IsString()
+    @IsNotEmpty()
+    telefono:string;
+    
     email:string;
+    
     mensaje:string;
 }
