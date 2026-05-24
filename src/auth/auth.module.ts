ed20ea17780227bcd9ac1ca2162dcdd6233974a7
Juan Campos Rey <juanccamposrey@gmail.com>
2026-05-24 19:57:32 -0300

    se corrige un error de add .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,13 +8,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [AdminModule,ComerciosModule,PassportModule.register({defaultStrategy: 'jwt'}),
+  imports: [AdminModule, ComerciosModule, PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'Olavarria_Conecta_2026', // Poné la
+      secret: 'Olavarria_Conecta_2026', // 
       signOptions: { expiresIn: '8h' }, // El token expira en 8 horas
     }),
   ], // Importante para que Auth pueda usar AdminService
-  providers: [AuthService],
-  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController], 
+  exports: [AuthService, PassportModule, JwtModule]
 })
-export class AuthModule {}
+export class AuthModule { }
