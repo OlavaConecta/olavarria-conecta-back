062009f4c5ae689f9baddcaf913ad0845a0cc47a
Juan Campos Rey <juanccamposrey@gmail.com>
2026-05-24 20:38:05 -0300

    asdasd1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,8 +11,9 @@ export class AuthController {
   }
 
   // --- LOGIN PARA COMERCIOS (Tecnomanía y otros) ---
-  @Post('login-comercio') // Ruta: http://localhost:3000/auth/login-comercio
+@Post('login-comercio') // Ruta: https://.../auth/login-comercio
   async loginComercio(@Body() body: { nombreUsuario: string, contrasena: string }) {
-    return this.authService.validateComercio(body.nombreUsuario, body.contrasena);
+    // 🔥 CORREGIDO: Ahora llamamos al método que genera y mete el TOKEN JWT
+    return this.authService.loginComercio(body.nombreUsuario, body.contrasena);
   }
 }
